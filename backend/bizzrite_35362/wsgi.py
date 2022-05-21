ac516a729a8729191964e6756bd683ec7b1b3280
Crowdbotics <team@crowdbotics.com>
2022-05-21 13:28:59 +0000

    Update models

--- backend/bizzrite_35362/wsgi.py
+++ backend/bizzrite_35362/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bizzrite_35362.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bizzrite_35362.settings")
 
 application = get_wsgi_application()
